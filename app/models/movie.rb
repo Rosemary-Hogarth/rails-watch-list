9b2441bfd1cadfa5b2980eefd67066eda23b81a2
Rosemary Hogarth <rohogarth@gmail.com>
2024-05-19 13:44:09 +0200

    Basic users stories and styling

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, dependent: :delete_all
+  has_many :lists, through: :bookmarks
+
   validates :title, presence: true, uniqueness: true
-  validates :overview, presence: true
+  validates :overview, :poster_url, :rating, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,58 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'open-uri'
+require 'json'
+
+puts "Cleaning database..."
+Movie.destroy_all if Rails.env.development?
+List.destroy_all if Rails.env.development?
+Bookmark.destroy_all if Rails.env.development?
+
+def create_lists
+  puts "creating lists..."
+  list_names = ["My watch list", "Must-watch classics", "Favourites", "Movies for holidays", "Ones I missed"]
+  lists = list_names.map do |list_name|
+    List.create!(name: list_name)
+  end
+  puts "Finished creating lists!"
+  lists # Return the array of list records
+end
+
+def fetch_movies(url)
+  puts "Fetching movies from API..."
+  url = 'https://tmdb.lewagon.com/movie/top_rated'
+  movies_serialized = URI.open(url).read
+  JSON.parse(movies_serialized)
+end
+
+def create_movies(movies_data)
+  puts "creating movies..."
+  movies_data['results'].each do |movie|
+    Movie.create!(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
+      rating: movie['vote_average']
+    )
+  end
+  puts "Finished!"
+end
+
+
+# assign the fetch to a variable
+movies_data = fetch_movies('https://tmdb.lewagon.com/movie/top_rated')
+create_movies(movies_data)
+lists = create_lists # Capturing the lists returned by create_lists
+movies = Movie.all
+# create bookmarks for each list
+puts "Creating bookmarks..."
+number_of_movies_per_list = 3
+
+lists.each_with_index do |list, index|
+  # Selecting a range of movies based on the current index
+  # This creates a range that does not overlap with other lists
+  movie_subset = movies.drop(index * number_of_movies_per_list).first(number_of_movies_per_list)
+
+  movie_subset.each do |movie|
+    Bookmark.create!(list: list, movie: movie, comment: "Great movie!") # Creates a bookmark for each movie in the list
+  end
+end
+puts "Finished creating bookmarks!"
