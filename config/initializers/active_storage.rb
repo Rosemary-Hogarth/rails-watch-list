ca35f4b85eb111a5d9ab2182aae34b1e7a0b29e1
Rosemary Hogarth <rohogarth@gmail.com>
2024-05-24 19:51:43 +0200

    fixed environment

--- config/initializers/active_storage.rb
+++
@@ -1,8 +0,0 @@
-# config/initializers/active_storage.rb
-
-Rails.application.config.after_initialize do
-  ActiveStorage::Current.url_options = {
-    host: 'watch-list-app-2da478c9c126.herokuapp.com',
-    protocol: 'https'
-  }
-end
